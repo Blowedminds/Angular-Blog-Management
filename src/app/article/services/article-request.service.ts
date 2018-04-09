d599d5b9787e9cef150fdb28d113929786fafacd
blowedminds <ruzgardeniz.08@hotmail.com>
2018-04-09 15:38:20 +0300

    Code clean up

--- src/app/article/services/article-request.service.ts
+++ src/app/article/services/article-request.service.ts
@@ -13,145 +13,130 @@ export class ArticleRequestService extends MainRequestService {
     http: HttpClient,
     helpersService: HelpersService,
     routingListService: RoutingListService
-  )
-  {
+  ) {
     super(http, helpersService, routingListService);
   }
 
-  getArticle(slug: string): Observable<any>
-  {
+  getArticle(slug: string): Observable<any> {
     const url = this.makeUrl('article.article', slug);
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getArticleByContent(slug: string, language_slug: string): Observable<any>
-  {
+  getArticleByContent(slug: string, language_slug: string): Observable<any> {
     const url = this.makeUrl('article.article.content', `${slug}/${language_slug}`);
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getArticles(): Observable<any>
-  {
+  getArticles(): Observable<any> {
     return this.http
-                    .get(this.makeUrl('articles'), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(this.makeUrl('articles'), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getArticlesPaginate(paginate: any): Observable<any>
-  {
+  getArticlesPaginate(paginate: any): Observable<any> {
     const url = this.makeUrl('article.articles', `?page=${paginate.pageIndex}&per-page=${paginate.pageSize}`);
 
     return this.http
-                    .get(url,  this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  putArticle(data: any): Observable<any>
-  {
+  putArticle(data: any): Observable<any> {
     const url = this.makeUrl('article.article');
 
     data.category = JSON.stringify(data.category);
 
     return this.http
-                    .put(url, JSON.stringify(data), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .put(url, JSON.stringify(data), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  postArticleContent(id: number, data: any): Observable<any>
-  {
+  postArticleContent(id: number, data: any): Observable<any> {
     const url = this.makeUrl('article.article.content', `${id}`);
 
-    let formData = new FormData()
+    const formData = new FormData();
 
     return this.http
-                    .post(url, JSON.stringify(data), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .post(url, JSON.stringify(data), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  putArticleContent(id: number, data: any): Observable<any>
-  {
+  putArticleContent(id: number, data: any): Observable<any> {
     const url = this.makeUrl('article.article.content', `${id}`);
 
-    const formData = new FormData()
+    const formData = new FormData();
 
     return this.http
-                    .put(url, JSON.stringify(data), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .put(url, JSON.stringify(data), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  postArticle(id: string, data: any): Observable<any>
-  {
+  postArticle(id: string, data: any): Observable<any> {
     const url = this.makeUrl('article.article', `${id}`);
 
     return this.http
-                    .post(url, JSON.stringify(data), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .post(url, JSON.stringify(data), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  deleteArticle(id: number): Observable<any>
-  {
+  deleteArticle(id: number): Observable<any> {
     const url = this.makeUrl('article.article', `${id}`);
 
     return this.http
-                    .delete(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .delete(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getTrash(): Observable<any>
-  {
+  getTrash(): Observable<any> {
     const url = this.makeUrl('article.trash');
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  postRestore(id: number): Observable<any>
-  {
+  postRestore(id: number): Observable<any> {
     const url = this.makeUrl('article.article.restore', `${id}`);
 
     return this.http
-                    .post(url, null, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .post(url, null, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  deleteForceDelete(id: number): Observable<any>
-  {
+  deleteForceDelete(id: number): Observable<any> {
     const url = this.makeUrl('article.article.force-delete', `${id}`);
 
     return this.http
-                    .delete(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .delete(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
-  getPermission(article_id: number): Observable<any>
-  {
+  getPermission(article_id: number): Observable<any> {
     const url = this.makeUrl('article.article.permission', `${article_id}`);
 
     return this.http
-                    .get(url, this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .get(url, this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 
   putPermission(
     article_id: number,
     change_have_permission: Array<any>,
     change_not_have_permission: Array<any>
-  ): Observable<any>
-  {
-    const url = this.makeUrl('article.permission',`${article_id}`);
+  ): Observable<any> {
+    const url = this.makeUrl('article.permission', `${article_id}`);
 
     return this.http
-                    .put(url, JSON.stringify({
-                      have_permission: change_have_permission,
-                      not_have_permission: change_not_have_permission
-                    }), this.options)
-                    .pipe(catchError(error => this.handleError(error)));
+      .put(url, JSON.stringify({
+        have_permission: change_have_permission,
+        not_have_permission: change_not_have_permission
+      }), this.options)
+      .pipe(catchError(error => this.handleError(error)));
   }
 }
